bc77afa925b3b0f722b04c16eeb2ddf86a25c114
QuiteSurreal <82947991+QuiteSurreal@users.noreply.github.com>
2026-05-01 20:23:05 +0200

    forgot about the other run

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ source detectVenv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 
-uvicorn app.routes:app --reload --host 0.0.0.0 --port 8000
+uvicorn app.routes:app --reload
